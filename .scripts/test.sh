37148aca440b5e1c8fa3599e1fcbd8715d538071
ocavue <ocavue@gmail.com>
2024-01-16 01:34:07 +0800

    ci: execute workflows in parallel

--- .scripts/test.sh
+++ .scripts/test.sh
@@ -3,44 +3,43 @@ set -e
 cd $(dirname $0)/..
 ROOT=$(pwd)
 
-# Set the default value for $1 to "pnpm" if it's not provided
-PACKAGE_MANAGER="${1:-pnpm}"
+PACKAGE_MANAGER="${1}"
+echo "Using package manager $PACKAGE_MANAGER"
+
+TEST_DIR="${2}"
+echo "Testing directory $TEST_DIR"
 
 mkdir -p ./.temp
 rm -rf ./.temp
 
-for file in $(find "$ROOT" -name 'package.json' -not -path '*/node_modules/*' | sort)
-do 
-    cd $(dirname $file)
-    echo "Testing $PWD"
+cd "$TEST_DIR"
 
-    # Ignore some examples that are not working yet
-    base=$(basename "$PWD")
-    if [[ "$base" == "vue-code-block" ]]; then
-        continue
-    fi
-    if [[ "$base" == "react-code-block" ]]; then
-        continue
-    fi
-    if [[ "$base" == "lit-dom" ]]; then
-        continue
-    fi
-    if [[ "$base" == "react-slash-menu" ]]; then
-        continue
-    fi
-    if [[ "$base" == "vue-full" ]]; then
-        continue
-    fi
-    if [[ "$base" == "vue-inline-menu" ]]; then
-        continue
-    fi
-    if [[ "$base" == "vue-slash-menu" ]]; then
-        continue
-    fi
-    if [[ "$base" == "vue-keymap" ]]; then
-        continue
-    fi
+# Ignore some examples that are not working yet
+base=$(basename "$PWD")
+if [[ "$base" == "vue-code-block" ]]; then
+    exit 0
+fi
+if [[ "$base" == "react-code-block" ]]; then
+    exit 0
+fi
+if [[ "$base" == "lit-dom" ]]; then
+    exit 0
+fi
+if [[ "$base" == "react-slash-menu" ]]; then
+    exit 0
+fi
+if [[ "$base" == "vue-full" ]]; then
+    exit 0
+fi
+if [[ "$base" == "vue-inline-menu" ]]; then
+    exit 0
+fi
+if [[ "$base" == "vue-slash-menu" ]]; then
+    exit 0
+fi
+if [[ "$base" == "vue-keymap" ]]; then
+    exit 0
+fi
 
-    $PACKAGE_MANAGER install --prefer-offline
-    $PACKAGE_MANAGER run build 
-done
+$PACKAGE_MANAGER install --prefer-offline
+$PACKAGE_MANAGER run build 
