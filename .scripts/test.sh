b6fb90729247a1082c6ec9cd9be155225840c06c
ocavue <ocavue@gmail.com>
2024-01-16 02:43:11 +0800

    ci: enable all examples

--- .scripts/test.sh
+++ .scripts/test.sh
@@ -20,33 +20,6 @@ for TEST_DIR in $TEST_DIRS; do
     rm -rf ./.temp
     mkdir -p ./.temp
 
-    # Ignore some examples that are not working yet
-    base=$(basename "$PWD")
-    if [[ "$base" == "vue-code-block" ]]; then
-        continue
-    fi
-    if [[ "$base" == "react-code-block" ]]; then
-        continue
-    fi
-    if [[ "$base" == "lit-dom" ]]; then
-        continue
-    fi
-    if [[ "$base" == "react-slash-menu" ]]; then
-        continue
-    fi
-    if [[ "$base" == "vue-full" ]]; then
-        continue
-    fi
-    if [[ "$base" == "vue-inline-menu" ]]; then
-        continue
-    fi
-    if [[ "$base" == "vue-slash-menu" ]]; then
-        continue
-    fi
-    if [[ "$base" == "vue-keymap" ]]; then
-        continue
-    fi
-
     $PACKAGE_MANAGER install --prefer-offline
     $PACKAGE_MANAGER run build 
 done
