b20a4a061863b52e0691e040fc9112f0f7eeedb0
ocavue <ocavue@gmail.com>
2024-01-16 02:29:25 +0800

    w

--- .scripts/collect_dirs.sh
+++
@@ -1,8 +0,0 @@
-# Output all directories with package.json files in them, in JSON format.
-
-set -e 
-
-cd $(dirname $0)/..
-ROOT=$(pwd)
-
-find "$ROOT" -name 'package.json' -not -path '*/node_modules/*' | sort | xargs dirname | jq --raw-input --slurp --compact-output 'split("\n")[:-1]'
