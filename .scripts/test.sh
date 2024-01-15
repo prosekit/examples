110e367390e1cf1a1d1df2093d4ef9280012d46c
ocavue <ocavue@gmail.com>
2024-01-16 03:06:45 +0800

    add pnpm version

--- .scripts/test.sh
+++ .scripts/test.sh
@@ -8,6 +8,7 @@ ROOT=$(pwd)
 # First argument is the package manager to use
 PACKAGE_MANAGER="${1}"
 echo "Using package manager $PACKAGE_MANAGER"
+$PACKAGE_MANAGER --version
 
 # Rest of the arguments are the directories to test
 TEST_DIRS="${@:2}"
