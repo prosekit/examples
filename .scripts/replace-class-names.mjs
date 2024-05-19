50b47092ca9ee794d989e5980cf1e64009cf68a3
ocavue <ocavue@gmail.com>
2024-05-19 15:32:47 +1000

    feat: use new replace function

--- .scripts/replace-class-names.mjs
+++ .scripts/replace-class-names.mjs
@@ -1,12 +1,12 @@
 import fs from 'node:fs/promises'
 
-import { replaceShortcuts } from '../.temp/prosekit/config/unocss-shortcut.mjs'
+import { replaceThemes } from '../.temp/prosekit/packages/themes/dist/prosekit-themes.gen.js'
 
 async function main() {
   // get the file name for args
   const fileName = process.argv[2]
   const text = await fs.readFile(fileName, 'utf-8')
-  const newText = replaceShortcuts(text)
+  const newText = replaceThemes(text)
   if (text !== newText) {
     await fs.writeFile(fileName, newText)
   }
