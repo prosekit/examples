f8e9710b3d37646cac3f243177421a196dccf51f
ocavue <ocavue@gmail.com>
2024-06-21 08:29:50 +1000

    feat: update pr script

--- .scripts/create-pr.sh
+++ .scripts/create-pr.sh
@@ -8,14 +8,19 @@ ROOT=$(pwd)
 # Get the target from the first argument. Default to "latest"
 target="${1:-latest}"
 
+git checkout master
+branch_local="update-$target-$(date +%Y%m%d%H%M%S)"
+branch_remote="update-$target"
+
+git checkout -b "$branch_local"
+
 for temp_dir in $(ls -d $ROOT/.templates/template*); do
     cd $temp_dir
     echo "Updating $(dirname $temp_dir)"
     pnpm add prosekit@$target
 done
-
-git config --add --bool push.autoSetupRemote true
-git checkout -b update-prosekit-$target-$(date +%Y%m%d%H%M%S)
 git commit -am "feat: update prosekit to $target"
-git push
+git push origin -f HEAD:"$branch_remote"
+git checkout $branch_remote
+git reset --hard origin/$branch_remote
 gh pr create --draft --title "feat: update prosekit to $target" --body "Update prosekit to $target"
