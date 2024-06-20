73b75aa7daf0a38b2a98b19cec974700e08b1abc
ocavue <ocavue@gmail.com>
2024-06-21 08:21:24 +1000

    fix

--- .scripts/create-pr.sh
+++ .scripts/create-pr.sh
@@ -18,4 +18,4 @@ git config --add --bool push.autoSetupRemote true
 git checkout -b update-prosekit-$target-$(date +%Y%m%d%H%M%S)
 git commit -am "Update prosekit to $target"
 git push --set-upstream origin update-prosekit-$target-$(date +%Y%m%d%H%M%S)
-gh pr create --title "feat: update prosekit to $target"
+gh pr create --fill --draft
