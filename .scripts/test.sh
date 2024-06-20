efc6460eca4c5ba8f2aa647b68014ba19d1416d3
ocavue <ocavue@gmail.com>
2024-06-21 08:50:58 +1000

    fix: add yarn-deduplicate to fix broken versions

--- .scripts/test.sh
+++ .scripts/test.sh
@@ -22,5 +22,18 @@ for TEST_DIR in $TEST_DIRS; do
     cd "$TEST_DIR"
 
     $PACKAGE_MANAGER install --prefer-offline
+
+    # If the package manager is yarn v1, we need to run special commands to 
+    # ensure that the version range "^0.0.0-next" won't install "0.0.0".
+    if [ "$PACKAGE_MANAGER" = "yarn" ]; then
+        # Install yarn-deduplicate if it's not already installed
+        if ! yarn-deduplicate --version > /dev/null 2>&1; then
+            echo "Installing yarn-deduplicate"
+            yarn global add yarn-deduplicate
+        fi
+        yarn-deduplicate --strategy fewer
+        yarn install --prefer-offline
+    fi
+
     $PACKAGE_MANAGER run build 
 done
