4ee29fbf24e2eda11dd2dd6802977ef4add69d10
ocavue <ocavue@users.noreply.github.com>
2024-06-23 19:08:08 +1000

    feat: add template-nuxt (#85)

---
+++ nuxt-full/src/code-block-view.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import type { CodeBlockAttrs } from 'prosekit/extensions/code-block'
+import type { VueNodeViewProps } from 'prosekit/vue'
+import { computed } from 'vue'
+import LanguageSelector from './language-selector.vue'
+
+const props = defineProps<VueNodeViewProps>()
+
+const language = computed(() => {
+  const attrs = props.node.value.attrs as CodeBlockAttrs
+  return attrs.language
+})
+
+const setLanguage = (language: string) => {
+  const attrs: CodeBlockAttrs = { language }
+  props.setAttrs(attrs)
+}
+</script>
+
+<template>
+  <LanguageSelector :language="language" :setLanguage="setLanguage" />
+  <pre :ref="props.contentRef" :data-language="language"></pre>
+</template>
---
+++ nuxt-full/src/extension.ts
@@ -0,0 +1,28 @@
+import { defineBasicExtension } from 'prosekit/basic'
+import { union } from 'prosekit/core'
+import {
+  defineCodeBlock,
+  defineCodeBlockShiki,
+} from 'prosekit/extensions/code-block'
+import { defineMention } from 'prosekit/extensions/mention'
+import { definePlaceholder } from 'prosekit/extensions/placeholder'
+import { defineVueNodeView, type VueNodeViewComponent } from 'prosekit/vue'
+
+import CodeBlockView from './code-block-view.vue'
+
+export function defineExtension() {
+  return union([
+    defineBasicExtension(),
+    definePlaceholder({ placeholder: 'Press / for commands...' }),
+    defineMention(),
+    defineCodeBlock(),
+    defineCodeBlockShiki(),
+    defineVueNodeView({
+      name: 'codeBlock',
+      contentAs: 'code',
+      component: CodeBlockView as VueNodeViewComponent,
+    }),
+  ])
+}
+
+export type EditorExtension = ReturnType<typeof defineExtension>
