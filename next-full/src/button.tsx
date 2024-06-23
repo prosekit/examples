11177f10c4f8a9335f89e177449167b834830bcc
ocavue <ocavue@users.noreply.github.com>
2024-06-23 19:48:06 +1000

    feat: add template-next (#89)

---
+++ next-full/src/button.tsx
@@ -0,0 +1,44 @@
+import {
+  TooltipContent,
+  TooltipRoot,
+  TooltipTrigger,
+} from 'prosekit/react/tooltip'
+import { useState, type ReactNode } from 'react'
+
+export default function Button({
+  pressed,
+  disabled,
+  onClick,
+  tooltip,
+  children,
+}: {
+  pressed?: boolean
+  disabled?: boolean
+  onClick?: VoidFunction
+  tooltip?: string
+  children: ReactNode
+}) {
+  const [tooltipOpen, setTooltipOpen] = useState(false)
+
+  return (
+    <TooltipRoot open={tooltipOpen} onOpenChange={setTooltipOpen}>
+      <TooltipTrigger className='block'>
+        <button
+          data-state={pressed ? 'on' : 'off'}
+          disabled={disabled}
+          onClick={() => onClick?.()}
+          onMouseDown={(event) => event.preventDefault()}
+          className='outline-unset focus-visible:outline-unset flex items-center justify-center rounded-md p-2 font-medium transition focus-visible:ring-2 text-sm focus-visible:ring-zinc-900 dark:focus-visible:ring-zinc-300 disabled:pointer-events-none min-w-9 min-h-9 disabled:opacity-50 hover:disabled:opacity-50 bg-transparent hover:bg-zinc-100 dark:hover:bg-zinc-800 data-[state=on]:bg-gray-200 dark:data-[state=on]:bg-gray-700'
+        >
+          {children}
+          {tooltip ? <span className="sr-only">{tooltip}</span> : null}
+        </button>
+      </TooltipTrigger>
+      {tooltip && !disabled && tooltipOpen ? (
+        <TooltipContent className='z-50 overflow-hidden rounded-md border border-solid bg-zinc-900 dark:bg-zinc-50 px-3 py-1.5 text-xs text-zinc-50 dark:text-zinc-900 shadow-sm will-change-transform data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=open]:fade-in-0 data-[state=closed]:fade-out-0 data-[state=open]:zoom-in-95 data-[state=closed]:zoom-out-95 data-[state=open]:animate-duration-150 data-[state=closed]:animate-duration-200 data-[side=bottom]:slide-in-from-top-2 data-[side=bottom]:slide-out-to-top-2 data-[side=left]:slide-in-from-right-2 data-[side=left]:slide-out-to-right-2 data-[side=right]:slide-in-from-left-2 data-[side=right]:slide-out-to-left-2 data-[side=top]:slide-in-from-bottom-2 data-[side=top]:slide-out-to-bottom-2'>
+          {tooltip}
+        </TooltipContent>
+      ) : null}
+    </TooltipRoot>
+  )
+}
