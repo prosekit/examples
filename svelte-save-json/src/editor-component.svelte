5f64f7f7b8fe8c1a34dee40f87af77f75d8ca6ed
ocavue <ocavue@users.noreply.github.com>
2024-07-08 03:51:38 +1000

    feat: update prosekit to latest (#100)

---
+++ svelte-save-json/src/editor-component.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+import 'prosekit/basic/style.css'
+import { useDocChange } from 'prosekit/svelte'
+import type { Editor } from 'prosekit/core'
+
+export let editor: Editor
+export let onDocChange: () => void
+
+useDocChange(
+  () => {
+    onDocChange?.()
+  },
+  { editor },
+)
+
+const mount = (element: HTMLElement) => {
+  editor.mount(element)
+  return { destroy: () => editor.unmount() }
+}
+</script>
+
+<div class='relative w-full flex-1 box-border overflow-y-scroll'>
+  <div use:mount class='ProseMirror box-border min-h-full px-[max(40px,_calc(50%-330px))] py-[24px] outline-none outline-0 [&_span[data-mention="user"]]:text-blue-500 [&_span[data-mention="tag"]]:text-violet-500 [&_pre]:text-white [&_pre]:bg-zinc-800'></div>
+</div>
