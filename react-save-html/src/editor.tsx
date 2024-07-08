50378c341e3d5c2da1ddb97b3575d8992bce4248
ocavue <ocavue@users.noreply.github.com>
2024-07-08 16:07:14 +1000

    feat: update examples (#101)

--- react-save-html/src/editor.tsx
+++ react-save-html/src/editor.tsx
@@ -3,17 +3,19 @@ import 'prosekit/basic/style.css'
 import { defineBasicExtension } from 'prosekit/basic'
 import {
   createEditor,
-  jsonFromHTML,
   htmlFromNode,
+  jsonFromHTML,
   type NodeJSON,
 } from 'prosekit/core'
-import { ProseKit, useDocChange } from 'prosekit/react'
 import { useCallback, useMemo, useState } from 'react'
+
+import EditorComponent from './editor-component'
 
 export default function Editor() {
   const [defaultDoc, setDefaultDoc] = useState<NodeJSON | undefined>()
   const [records, setRecords] = useState<string[]>([])
   const [hasUnsavedChange, setHasUnsavedChange] = useState(false)
+  const [key, setKey] = useState(1)
 
   // Create a new editor instance whenever `defaultDoc` changes
   const editor = useMemo(() => {
@@ -23,8 +25,6 @@ export default function Editor() {
 
   // Enable the save button
   const handleDocChange = useCallback(() => setHasUnsavedChange(true), [])
-
-  useDocChange(handleDocChange, { editor })
 
   // Save the current document as a HTML string
   const handleSave = useCallback(() => {
@@ -38,39 +38,40 @@ export default function Editor() {
     (record: string) => {
       setDefaultDoc(jsonFromHTML(record, { schema: editor.schema }))
       setHasUnsavedChange(false)
+      setKey((key) => key + 1)
     },
     [records, editor.schema],
   )
 
   return (
-    <ProseKit editor={editor}>
-      <div className='box-border h-full w-full min-h-36 overflow-y-hidden overflow-x-hidden rounded-md border border-solid border-gray-200 shadow dark:border-zinc-700 flex flex-col bg-white dark:bg-neutral-900'>
-        <button
-          onClick={handleSave}
-          disabled={!hasUnsavedChange}
-          className="m-1 border border-solid bg-white px-2 py-1 text-sm text-black disabled:cursor-not-allowed disabled:text-gray-500"
-        >
-          {hasUnsavedChange ? 'Save' : 'No changes to save'}
-        </button>
-        <ul className="border-b border-t border-solid text-sm">
-          {records.map((record, index) => (
-            <li key={index} className="m-1 flex gap-2">
-              <button
-                className="border border-solid bg-white px-2 py-1 text-black"
-                onClick={() => handleLoad(record)}
-              >
-                Load
-              </button>
-              <span className="flex-1 overflow-x-scroll p-2">
-                <pre>{record}</pre>
-              </span>
-            </li>
-          ))}
-        </ul>
-        <div className='relative w-full flex-1 box-border overflow-y-scroll'>
-          <div ref={editor.mount} className='ProseMirror box-border min-h-full px-[max(40px,_calc(50%-330px))] py-[24px] outline-none outline-0 [&_span[data-mention="user"]]:text-blue-500 [&_span[data-mention="tag"]]:text-violet-500 [&_pre]:text-white [&_pre]:bg-zinc-800'></div>
-        </div>
-      </div>
-    </ProseKit>
+    <div className='box-border h-full w-full min-h-36 overflow-y-hidden overflow-x-hidden rounded-md border border-solid border-gray-200 shadow dark:border-zinc-700 flex flex-col bg-white dark:bg-neutral-900'>
+      <button
+        onClick={handleSave}
+        disabled={!hasUnsavedChange}
+        className="m-1 border border-solid bg-white px-2 py-1 text-sm text-black disabled:cursor-not-allowed disabled:text-gray-500"
+      >
+        {hasUnsavedChange ? 'Save' : 'No changes to save'}
+      </button>
+      <ul className="border-b border-t border-solid text-sm">
+        {records.map((record, index) => (
+          <li key={index} className="m-1 flex gap-2">
+            <button
+              className="border border-solid bg-white px-2 py-1 text-black"
+              onClick={() => handleLoad(record)}
+            >
+              Load
+            </button>
+            <span className="flex-1 overflow-x-scroll p-2">
+              <pre>{record}</pre>
+            </span>
+          </li>
+        ))}
+      </ul>
+      <EditorComponent
+        key={key}
+        editor={editor}
+        onDocChange={handleDocChange}
+      />
+    </div>
   )
 }
--- svelte-save-json/src/editor-component.svelte
+++ svelte-save-json/src/editor-component.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 import 'prosekit/basic/style.css'
-import { useDocChange } from 'prosekit/svelte'
+import { useDocChange, ProseKit } from 'prosekit/svelte'
 import type { Editor } from 'prosekit/core'
 
 export let editor: Editor
@@ -19,6 +19,8 @@ const mount = (element: HTMLElement) => {
 }
 </script>
 
-<div class='relative w-full flex-1 box-border overflow-y-scroll'>
-  <div use:mount class='ProseMirror box-border min-h-full px-[max(40px,_calc(50%-330px))] py-[24px] outline-none outline-0 [&_span[data-mention="user"]]:text-blue-500 [&_span[data-mention="tag"]]:text-violet-500 [&_pre]:text-white [&_pre]:bg-zinc-800'></div>
-</div>
+<ProseKit {editor}>
+  <div class='relative w-full flex-1 box-border overflow-y-scroll'>
+    <div use:mount class='ProseMirror box-border min-h-full px-[max(40px,_calc(50%-330px))] py-[24px] outline-none outline-0 [&_span[data-mention="user"]]:text-blue-500 [&_span[data-mention="tag"]]:text-violet-500 [&_pre]:text-white [&_pre]:bg-zinc-800'></div>
+  </div>
+</ProseKit>
