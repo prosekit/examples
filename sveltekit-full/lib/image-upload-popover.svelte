d61674483b6d8e137a9e968d3d6c24b2c2a90a2c
ocavue <ocavue@gmail.com>
2024-07-13 20:57:06 +1000

    copy code

---
+++ sveltekit-full/lib/image-upload-popover.svelte
@@ -0,0 +1,97 @@
+<script lang="ts">
+import { useEditor } from 'prosekit/svelte'
+import {
+  PopoverContent,
+  PopoverRoot,
+  PopoverTrigger,
+} from 'prosekit/svelte/popover'
+import Button from './button.svelte'
+import type { EditorExtension } from './extension'
+
+export let disabled: boolean
+export let tooltip: string
+
+let open = false
+let webUrl = ''
+let objectUrl = ''
+$: url = webUrl || objectUrl
+const editor = useEditor<EditorExtension>()
+
+const handleFileChange = (event: Event) => {
+  const file = (event.target as HTMLInputElement)?.files?.[0]
+
+  if (file) {
+    objectUrl = URL.createObjectURL(file)
+    webUrl = ''
+  } else {
+    objectUrl = ''
+  }
+}
+
+const handleWebUrlChange = (event: Event) => {
+  const url = (event.target as HTMLInputElement)?.value
+
+  if (url) {
+    webUrl = url
+    objectUrl = ''
+  } else {
+    webUrl = ''
+  }
+}
+
+const deferResetState = () => {
+  setTimeout(() => {
+    webUrl = ''
+    objectUrl = ''
+  }, 300)
+}
+
+const handleSubmit = () => {
+  $editor.commands.insertImage({ src: url })
+  deferResetState()
+  open = false
+}
+
+const handleOpenChange = (openValue: boolean) => {
+  if (!openValue) {
+    deferResetState()
+  }
+  open = openValue
+}
+</script>
+
+<PopoverRoot {open} on:OpenChange={handleOpenChange}>
+  <PopoverTrigger>
+    <Button pressed={open} {disabled} {tooltip}>
+      <slot />
+    </Button>
+  </PopoverTrigger>
+
+  <PopoverContent class='flex flex-col gap-y-4 p-6 text-sm w-sm z-10 box-border rounded-lg border border-zinc-200 dark:border-zinc-800 bg-white dark:bg-neutral-900 shadow-lg will-change-transform data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=open]:fade-in-0 data-[state=closed]:fade-out-0 data-[state=open]:zoom-in-95 data-[state=closed]:zoom-out-95 data-[state=open]:animate-duration-150 data-[state=closed]:animate-duration-200 data-[side=bottom]:slide-in-from-top-2 data-[side=bottom]:slide-out-to-top-2 data-[side=left]:slide-in-from-right-2 data-[side=left]:slide-out-to-right-2 data-[side=right]:slide-in-from-left-2 data-[side=right]:slide-out-to-left-2 data-[side=top]:slide-in-from-bottom-2 data-[side=top]:slide-out-to-bottom-2'>
+    {#if !objectUrl}
+      <label for="embed-link-input">Embed Link</label>
+      <input
+        class='flex h-9 rounded-md w-full bg-white dark:bg-neutral-900 px-3 py-2 text-sm placeholder:text-zinc-500 dark:placeholder:text-zinc-500 transition border box-border border-zinc-200 dark:border-zinc-800 border-solid ring-0 ring-transparent focus-visible:ring-2 focus-visible:ring-zinc-900 dark:focus-visible:ring-zinc-300 focus-visible:ring-offset-0 outline-none focus-visible:outline-none file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:cursor-not-allowed disabled:opacity-50'
+        placeholder="Paste the image link..."
+        type="url"
+        value={webUrl}
+        on:input={handleWebUrlChange}
+        id="embed-link-input"
+      />
+    {/if}
+    {#if !webUrl}
+      <label for="upload-input">Upload</label>
+      <input
+        class='flex h-9 rounded-md w-full bg-white dark:bg-neutral-900 px-3 py-2 text-sm placeholder:text-zinc-500 dark:placeholder:text-zinc-500 transition border box-border border-zinc-200 dark:border-zinc-800 border-solid ring-0 ring-transparent focus-visible:ring-2 focus-visible:ring-zinc-900 dark:focus-visible:ring-zinc-300 focus-visible:ring-offset-0 outline-none focus-visible:outline-none file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:cursor-not-allowed disabled:opacity-50'
+        accept="image/*"
+        type="file"
+        on:input={handleFileChange}
+      />
+    {/if}
+    {#if url}
+      <button class='inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-white dark:ring-offset-neutral-900 transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-zinc-900 dark:focus-visible:ring-zinc-300 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-zinc-900 dark:bg-zinc-50 text-zinc-50 dark:text-zinc-900 hover:bg-zinc-900/90 dark:hover:bg-zinc-50/90 h-10 px-4 py-2 w-full' on:click={handleSubmit}>
+        Insert Image
+      </button>
+    {/if}
+  </PopoverContent>
+</PopoverRoot>
---
+++ sveltekit-full/lib/toolbar.svelte
@@ -0,0 +1,161 @@
+<script lang="ts">
+import { useEditor } from 'prosekit/svelte'
+import Button from './button.svelte'
+import type { EditorExtension } from './extension'
+import ImageUploadPopover from './image-upload-popover.svelte'
+
+const editor = useEditor<EditorExtension>({ update: true })
+</script>
+
+<div class='z-2 box-border border-zinc-200 dark:border-zinc-800 border-solid border-l-0 border-r-0 border-t-0 border-b flex flex-wrap gap-1 p-2 items-center'>
+  <Button
+    pressed={false}
+    disabled={!$editor.commands.undo.canApply()}
+    onClick={() => $editor.commands.undo()}
+    tooltip="Undo"
+  >
+    <div class='i-lucide-undo-2 h-5 w-5' />
+  </Button>
+
+  <Button
+    pressed={false}
+    disabled={!$editor.commands.redo.canApply()}
+    onClick={() => $editor.commands.redo()}
+    tooltip="Redo"
+  >
+    <div class='i-lucide-redo-2 h-5 w-5' />
+  </Button>
+
+  <Button
+    pressed={$editor.marks.bold.isActive()}
+    disabled={!$editor.commands.toggleBold.canApply()}
+    onClick={() => $editor.commands.toggleBold()}
+    tooltip="Bold"
+  >
+    <div class='i-lucide-bold h-5 w-5'></div>
+  </Button>
+
+  <Button
+    pressed={$editor.marks.italic.isActive()}
+    disabled={!$editor.commands.toggleItalic.canApply()}
+    onClick={() => $editor.commands.toggleItalic()}
+    tooltip="Italic"
+  >
+    <div class='i-lucide-italic h-5 w-5'></div>
+  </Button>
+
+  <Button
+    pressed={$editor.marks.underline.isActive()}
+    disabled={!$editor.commands.toggleUnderline.canApply()}
+    onClick={() => $editor.commands.toggleUnderline()}
+    tooltip="Underline"
+  >
+    <div class='i-lucide-underline h-5 w-5'></div>
+  </Button>
+
+  <Button
+    pressed={$editor.marks.strike.isActive()}
+    disabled={!$editor.commands.toggleStrike.canApply()}
+    onClick={() => $editor.commands.toggleStrike()}
+    tooltip="Strike"
+  >
+    <div class='i-lucide-strikethrough h-5 w-5'></div>
+  </Button>
+
+  <Button
+    pressed={$editor.marks.code.isActive()}
+    disabled={!$editor.commands.toggleCode.canApply()}
+    onClick={() => $editor.commands.toggleCode()}
+    tooltip="Code"
+  >
+    <div class='i-lucide-code h-5 w-5'></div>
+  </Button>
+
+  <Button
+    pressed={$editor.nodes.heading.isActive({ level: 1 })}
+    disabled={!$editor.commands.toggleHeading.canApply({ level: 1 })}
+    onClick={() => $editor.commands.toggleHeading({ level: 1 })}
+    tooltip="Heading 1"
+  >
+    <div class='i-lucide-heading-1 h-5 w-5'></div>
+  </Button>
+
+  <Button
+    pressed={$editor.nodes.heading.isActive({ level: 2 })}
+    disabled={!$editor.commands.toggleHeading.canApply({ level: 2 })}
+    onClick={() => $editor.commands.toggleHeading({ level: 2 })}
+    tooltip="Heading 2"
+  >
+    <div class='i-lucide-heading-2 h-5 w-5'></div>
+  </Button>
+
+  <Button
+    pressed={$editor.nodes.heading.isActive({ level: 3 })}
+    disabled={!$editor.commands.toggleHeading.canApply({ level: 3 })}
+    onClick={() => $editor.commands.toggleHeading({ level: 3 })}
+    tooltip="Heading 3"
+  >
+    <div class='i-lucide-heading-3 h-5 w-5'></div>
+  </Button>
+
+  <Button
+    pressed={$editor.nodes.list.isActive({ kind: 'bullet' })}
+    disabled={!$editor.commands.toggleList.canApply({ kind: 'bullet' })}
+    onClick={() => $editor.commands.toggleList({ kind: 'bullet' })}
+    tooltip="Bullet List"
+  >
+    <div class='i-lucide-list h-5 w-5'></div>
+  </Button>
+
+  <Button
+    pressed={$editor.nodes.list.isActive({ kind: 'ordered' })}
+    disabled={!$editor.commands.toggleList.canApply({ kind: 'ordered' })}
+    onClick={() => $editor.commands.toggleList({ kind: 'ordered' })}
+    tooltip="Ordered List"
+  >
+    <div class='i-lucide-list-ordered h-5 w-5'></div>
+  </Button>
+
+  <Button
+    pressed={$editor.nodes.list.isActive({ kind: 'task' })}
+    disabled={!$editor.commands.toggleList.canApply({ kind: 'task' })}
+    onClick={() => $editor.commands.toggleList({ kind: 'task' })}
+    tooltip="Task List"
+  >
+    <div class='i-lucide-list-checks h-5 w-5'></div>
+  </Button>
+
+  <Button
+    pressed={$editor.nodes.list.isActive({ kind: 'toggle' })}
+    disabled={!$editor.commands.toggleList.canApply({ kind: 'toggle' })}
+    onClick={() => $editor.commands.toggleList({ kind: 'toggle' })}
+    tooltip="Toggle List"
+  >
+    <div class='i-lucide-list-collapse h-5 w-5'></div>
+  </Button>
+
+  <Button
+    pressed={false}
+    disabled={!$editor.commands.indentList.canApply()}
+    onClick={() => $editor.commands.indentList()}
+    tooltip="Increase indentation"
+  >
+    <div class='i-lucide-indent-increase h-5 w-5'></div>
+  </Button>
+
+  <Button
+    pressed={false}
+    disabled={!$editor.commands.dedentList.canApply()}
+    onClick={() => $editor.commands.dedentList()}
+    tooltip="Decrease indentation"
+  >
+    <div class='i-lucide-indent-decrease h-5 w-5'></div>
+  </Button>
+
+  <ImageUploadPopover
+    disabled={!$editor.commands.insertImage.canApply()}
+    tooltip="Insert Image"
+  >
+    <div class='i-lucide-image h-5 w-5'></div>
+  </ImageUploadPopover>
+</div>
