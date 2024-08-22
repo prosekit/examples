c38bcaf5ae6283c31eb5fbd8f720951841b6d199
ocavue <ocavue@gmail.com>
2024-08-22 20:03:45 +1000

    wip

---
+++ .overrides/react-loro/vite.config.ts
@@ -0,0 +1,10 @@
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import wasm from 'vite-plugin-wasm'
+
+
+
+
+export default defineConfig({
+  plugins: [react(), wasm()],
+})
