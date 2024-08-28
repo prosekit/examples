42ad45f6b272151487fe154a7328c8b0f6ca9734
ocavue <ocavue@gmail.com>
2024-08-28 23:18:31 +1000

    chore: rewrite the build script

---
+++ .scripts/replace-themes.ts
@@ -0,0 +1,4 @@
+// @ts-ignore: no types
+import { replaceThemes as _replaceThemes } from '../.temp/prosekit/packages/themes/dist/prosekit-themes.gen.js'
+
+export const replaceThemes: (text: string) => string = _replaceThemes
