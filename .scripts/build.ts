597f34e57cae11112b1764a4db5d272b3bb2ad04
ocavue <ocavue@gmail.com>
2024-09-01 19:57:09 +1000

    gen

--- .scripts/build.ts
+++ .scripts/build.ts
@@ -141,6 +141,8 @@ async function writeReadme(dir: string) {
 
   const text = `# ${name}
 
+A [ProseKit](https://prosekit.dev) example.
+
 [![Open in StackBlitz](https://developer.stackblitz.com/img/open_in_stackblitz.svg)](https://stackblitz.com/github/prosekit/examples/tree/master/${name})
 [![Open with CodeSandbox](https://assets.codesandbox.io/github/button-edit-lime.svg)](https://codesandbox.io/p/sandbox/github/prosekit/examples/tree/master/${name})
 
