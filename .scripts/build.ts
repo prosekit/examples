a3cfa14097e93e8e1202017d1b52eb1f94eae839
ocavue <ocavue@gmail.com>
2024-09-02 20:22:01 +1000

    feat: add online demo links

--- .scripts/build.ts
+++ .scripts/build.ts
@@ -146,14 +146,19 @@ A [ProseKit](https://prosekit.dev) example.
 [![Open in StackBlitz](https://developer.stackblitz.com/img/open_in_stackblitz.svg)](https://stackblitz.com/github/prosekit/examples/tree/master/${name})
 [![Open with CodeSandbox](https://assets.codesandbox.io/github/button-edit-lime.svg)](https://codesandbox.io/p/sandbox/github/prosekit/examples/tree/master/${name})
 
-Run the example locally with:
-
-\`\`\`bash
-npx degit prosekit/examples/${name} ${name}
-cd ${name}
-npm install
-npm run dev
-\`\`\`
+- Source code: https://github.com/prosekit/examples/tree/master/${name}
+- Online demo: https://prosekit.dev/astrobook/stories/${name.replace(
+    /^(\w+)-(.*)$/,
+    '$1/$2',
+  )}
+- Run the example locally:
+  
+  \`\`\`bash
+  npx degit prosekit/examples/${name} ${name}
+  cd ${name}
+  npm install
+  npm run dev
+  \`\`\`
 `
   await fs.writeFile(filePath, text)
 }
