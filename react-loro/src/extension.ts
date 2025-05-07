ccd3a5a73f086197174a3822efdc04c190673fd1
ocavue <ocavue@users.noreply.github.com>
2025-05-07 16:45:34 +1000

    chore(deps): update dependency prosekit to v0.13.0 (#747)

--- react-loro/src/extension.ts
+++ react-loro/src/extension.ts
@@ -1,15 +1,10 @@
 import type { CursorAwareness, LoroDocType } from 'loro-prosemirror'
-import {
-  defineBaseCommands,
-  defineBaseKeymap,
-  defineDoc,
-  defineParagraph,
-  defineText,
-  union,
-} from 'prosekit/core'
+import { defineBaseCommands, defineBaseKeymap } from 'prosekit/core'
+import { union } from 'prosekit/core'
 import { defineBlockquote } from 'prosekit/extensions/blockquote'
 import { defineBold } from 'prosekit/extensions/bold'
 import { defineCode } from 'prosekit/extensions/code'
+import { defineDoc } from 'prosekit/extensions/doc'
 import { defineDropCursor } from 'prosekit/extensions/drop-cursor'
 import { defineHeading } from 'prosekit/extensions/heading'
 import { defineImage } from 'prosekit/extensions/image'
@@ -18,8 +13,10 @@ import { defineLink } from 'prosekit/extensions/link'
 import { defineList } from 'prosekit/extensions/list'
 import { defineLoro } from 'prosekit/extensions/loro'
 import { defineModClickPrevention } from 'prosekit/extensions/mod-click-prevention'
+import { defineParagraph } from 'prosekit/extensions/paragraph'
 import { defineStrike } from 'prosekit/extensions/strike'
 import { defineTable } from 'prosekit/extensions/table'
+import { defineText } from 'prosekit/extensions/text'
 import { defineUnderline } from 'prosekit/extensions/underline'
 import { defineVirtualSelection } from 'prosekit/extensions/virtual-selection'
 
