d9391de01df3bf4964cf711b00538bb6321e55e3
ocavue <ocavue@gmail.com>
2025-11-07 14:42:42 +1100

    remove clone

--- .scripts/clone.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -ex 
-
-cd $(dirname $0)/..
-
-if [ ! -d ./.temp/prosekit ]; then
-  git clone --depth 1 https://github.com/ocavue/prosekit --branch master ./.temp/prosekit
-fi
