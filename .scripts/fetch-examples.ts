ed95c0004347454d7672e3aec0dc025dc93e160a
ocavue <ocavue@gmail.com>
2025-11-07 18:41:14 +1100

    fix

--- .scripts/fetch-examples.ts
+++ .scripts/fetch-examples.ts
@@ -57,7 +57,7 @@ const nextEntry = (story: string) => `'use client'
 
 import dynamic from 'next/dynamic'
 
-const EditorLazy = dynamic(() => import('./components/editor/examples/${story}').then(mod => { default: mod.ExampleEditor }), { ssr: false })
+const EditorLazy = dynamic(() => import('./components/editor/examples/${story}').then(mod => ({ default: mod.ExampleEditor })), { ssr: false })
 
 export default function Editor() {
   return <EditorLazy />
--- next-full/components/editor-dynamic.tsx
+++ next-full/components/editor-dynamic.tsx
@@ -2,7 +2,13 @@
 
 import dynamic from 'next/dynamic'
 
-const EditorLazy = dynamic(() => import('./components/editor/examples/full').then(mod => { default: mod.ExampleEditor }), { ssr: false })
+const EditorLazy = dynamic(
+  () =>
+    import('./components/editor/examples/full').then((mod) => ({
+      default: mod.ExampleEditor,
+    })),
+  { ssr: false },
+)
 
 export default function Editor() {
   return <EditorLazy />
