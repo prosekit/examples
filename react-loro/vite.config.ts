cd2ca9847e42d381d68237c81815802e55d1bd9d
ocavue <ocavue@gmail.com>
2025-11-07 15:22:21 +1100

    fix

--- react-loro/vite.config.ts
+++ react-loro/vite.config.ts
@@ -1,8 +1,8 @@
-import tailwindcss from '@tailwindcss/vite'
-import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
-import wasm from 'vite-plugin-wasm'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), wasm(), tailwindcss()],
+  plugins: [react(), tailwindcss()],
 })
