36350360bd02f01a6655e290ec0a055f74277cf0
ocavue <ocavue@gmail.com>
2025-11-11 01:59:03 +1100

    wip

--- preact-block-handle/src/components/editor/sample/default-content-drag-and-drop.ts
+++ preact-block-handle/src/components/editor/sample/default-content-drag-and-drop.ts
@@ -4,20 +4,7 @@ export const DEFAULT_DRAG_AND_DROP_CONTENT = `
 
 <p>Try dragging any paragraph or heading by clicking on the handle that appears on the left when you hover over it.</p>
 
-<h2>Getting Started</h2>
-
-<p>Hover over any block to see the drag handle appear. Click and drag to reorder content.</p>
-
-<p>This paragraph can be moved above or below other blocks.</p>
-
-<h2>Different Block Types</h2>
-
-<p>You can drag paragraphs, headings, lists, code blocks, and more.</p>
-
-<h3>Lists Work Too</h3>
-
-<ul>
-  <li>This entire list can be dragged</li>
+<h2>Getting Started</hgged</li>
   <li>Individual list items stay together</li>
   <li>Try moving this list around</li>
 </ul>
