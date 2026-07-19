4fd52b0eb5c9c3eb88286ad5b73ae797f351268e
ocavue <ocavue@users.noreply.github.com>
2026-07-20 03:51:09 +1000

    fix: disable nuxt auto-import (#1806)

--- nuxt-full/nuxt.config.ts
+++ nuxt-full/nuxt.config.ts
@@ -1,12 +1,14 @@
+import tailwindcss from '@tailwindcss/vite'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
   css: ['~/app.css'],
   srcDir: 'src',
-  postcss: {
-    plugins: {
-      '@tailwindcss/postcss': {},
-      autoprefixer: {},
-    },
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  imports: {
+    autoImport: false,
   },
 })
